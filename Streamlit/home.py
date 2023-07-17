da89c5754c946babb63f15cecca68b2880019718
johannesmichael <jroempp@ambergloglay.com>
2023-07-17 14:48:33 +0200

    add the streamlit interface

---
+++ Streamlit/home.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+
+
+#Page config
+st.set_page_config(page_title='Amberg Loglay Playground', page_icon='assets/AE logo only.png', layout='wide', initial_sidebar_state='expanded')
+
+#set up the title
+st.title('Amberg Loglay Playground for Model Evaluation')
+
+
+#markdown for sidebar main page
+
+st.sidebar.markdown("# HOME")
+
+
+
+
